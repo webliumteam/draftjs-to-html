ac90f1545ad72d0f6f964bbc28d3e59bd395c49b
vreznikweblium <v.reznik@weblium.com>
2019-05-14 18:41:17 +0300

    fix tagName

--- js/block.js
+++ js/block.js
@@ -4,7 +4,7 @@ import { forEach, isEmptyString } from './common';
 * Mapping block-type to corresponding html tag.
 */
 const blockTypesMapping: Object = {
-  unstyled: 'p',
+  unstyled: 'span',
   'header-one': 'h1',
   'header-two': 'h2',
   'header-three': 'h3',
